d4643d576942bc48b4282f8f58bae1198bc6e3f6
LauSCaicedo <3383@holbertonschool.com>
2021-10-23 02:15:02 -0500

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,16 +8,16 @@
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
-    int temp, *max, *min;
+    int temp, *xp, *yp;
     for (i = 0; i < size - 1; i++)
     {
         for (j = 0; j < size - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
-                temp = *max;
-                *max = *min;
-                *min = temp;
+                temp = *xp;
+                *xp = *yp;
+                *yp = temp;
                 print_array(array, size);
             }
         }
