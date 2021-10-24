4185ca7ac3a972db917b617364bbb0d35c2aa0ac
LauSCaicedo <3383@holbertonschool.com>
2021-10-24 02:19:47 -0500

    Update files

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,16 +1,4 @@
 #include "sort.h"
-/**
- * bubble - function that swap two elemts
- * @max: search the max number
- * @min: search the min number
- */
-
-void bubble(int *max, int *min)
-{
-	int temp = *max;
-	*max = *min;
-	*min = temp;
-}
 
 /**
  * bubble_sort - function that sorts an array of integers in ascending order
