3d5f9c0623ea845e50f14eca9727f5e7bf62709a
LauSCaicedo <3383@holbertonschool.com>
2021-10-26 19:37:51 -0500

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+
+/**
+ * bubble - Function that swap two elemts.
+ * @max: Search the max number.
+ * @min: Search the min number.
+ */
+
+void bubble(int *max, int *min)
+{
+	int temp = *max;
+	*max = *min;
+	*min = temp;
+}
+
+/**
+ * partition - This function is
+ * the one that actually sorts
+ * the array and exchanges values.
+ * @arr: Array of integers.
+ * @left: Firts position.
+ * @right: Last position.
+ * @size: Size of array.
+ * Return: Index in left.
+ */
+
+int partition(int *arr, int left, int right, int size)
+{
+	int pivot = arr[r];
+	int i = l - 1, j;
+
+	for (j = l; j <= r - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				bubble(&arr[i], &arr[j]);
+				print_array(arr, size);
+			}
+		}
+	}
+	if (i + 1 != r)
+	{
+		bubble(&arr[i + 1], &arr[j]);
+		print_array(arr, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * divide_conquer - Order with recursion.
+ * @array: Array of integer.
+ * @left: First position.
+ * @right: Last position.
+ * @size: Size of array.
+ * Return: Nothing.
+ */
+
+void divide_conquer(int array[], int left, int right, size_t size)
+{
+	int idx_part = 0;
+
+	if (left < right)
+	{
+		idx_part = partition(array, left, right, size);
+		divide_conquer(array, left, idx_part - 1, size);
+		divide_conquer(array, idx_part + 1, right, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of int in
+ * ascending order with Quick sort algorithm.
+ * @array: Array to sort.
+ * @size: Size to array.
+ * Return: Nothing.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	divide_conquer(array, 0, size - 1, size);
+}
