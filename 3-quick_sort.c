ef6f23ed1f625dad5c8ac82015e6d8084a0e9880
knia10 <kelly.stephania@hotmail.com>
2021-10-26 20:32:01 -0500

    modified array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,37 +14,52 @@ void bubble(int *max, int *min)
 }
 
 /**
+ * quick_sort - Sorts an array of int in
+ * ascending order with Quick sort algorithm.
+ * @array: Array to sort.
+ * @size: Size to array.
+ * Return: Nothing.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	divide_conquer(array, 0, size - 1, size);
+}
+
+/**
  * partition - This function is
  * the one that actually sorts
  * the array and exchanges values.
- * @arr: Array of integers.
+ * @array: Array of integers.
  * @left: Firts position.
  * @right: Last position.
  * @size: Size of array.
  * Return: Index in left.
  */
 
-int partition(int *arr, int left, int right, int size)
+int partition(int *array, int left, int right, int size)
 {
-	int pivot = arr[right];
+	int pivot = array[right];
 	int i = left - 1, j;
 
 	for (j = left; j <= right - 1; j++)
 	{
-		if (arr[j] < pivot)
+		if (array[j] < pivot)
 		{
 			i++;
 			if (i != j)
 			{
-				bubble(&arr[i], &arr[j]);
-				print_array(arr, size);
+				bubble(&array[i], &array[j]);
+				print_array(array, size);
 			}
 		}
 	}
 	if (i + 1 != right)
 	{
-		bubble(&arr[i + 1], &arr[j]);
-		print_array(arr, size);
+		bubble(&array[i + 1], &array[j]);
+		print_array(array, size);
 	}
 	return (i + 1);
 }
